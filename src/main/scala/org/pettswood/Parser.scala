72f10275584e54b3b0026e88c03f84826b4f6f39
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2011-10-30 11:18:30 +0000

    Made parser retain existing CSS classes in table cells

--- src/main/scala/org/pettswood/Parser.scala
+++ src/main/scala/org/pettswood/Parser.scala
@@ -6,23 +6,27 @@ class Parser(domain: DomainBridge) {
 
   def parse(test: Node): Node = traverseNodes(test)
 
-  def deepCopy(elem: Elem, attributes: (Elem) => MetaData = _.attributes): Elem = elem.copy(elem.prefix, elem.label, attributes(elem), TopScope, children(elem))
+  def deepCopy(elem: Elem, attributes: (Elem) => MetaData = _.attributes, text: String = ""): Elem = elem.copy(elem.prefix, elem.label, attributes(elem), TopScope, children(elem, text))
 
   def cssAdder(className: String): (Elem) => MetaData = {
-    (elem: Elem) => new UnprefixedAttribute("class", className, Null)
+    (elem: Elem) => {
+      val classes = (elem \ "@class").mkString match {
+        case "" => className
+        case x => x + " " + className
+      }
+      new UnprefixedAttribute("class", classes, Null)
+    }
   }
 
-  def copyAndChange(elem: Elem, colour: String): Elem = deepCopy(elem, cssAdder(colour))
-
-  def children(node: Node): NodeSeq = NodeSeq.fromSeq(node.child).map(child => traverseNodes(child))
+  def children(node: Node, textToAdd: String): NodeSeq = NodeSeq.fromSeq(node.child).map(child => traverseNodes(child))
   def firstCell(nodeSeq: NodeSeq): Elem = (nodeSeq \\ "td").head match { case elem: Elem => elem }
 
   def traverseNodes(currentNode: Node): Node = {
     currentNode match {
       case elem: Elem => elem.label match {
-        case "table" => {domain.table(firstCell(elem).text); deepCopy(elem)}
-        case "tr" => {domain.row(); deepCopy(elem)}
-        case "td" => var result = domain.cell(elem.text); copyAndChange(elem, result.name)
+        case "table" => domain.table(firstCell(elem).text); deepCopy(elem)
+        case "tr" => domain.row(); deepCopy(elem)
+        case "td" => val result = domain.cell(elem.text); deepCopy(elem, cssAdder(result.name))
         case _ => deepCopy(elem)
       }
       case any => any
--- src/test/scala/org/pettswood/ParserSpec.scala
+++ src/test/scala/org/pettswood/ParserSpec.scala
@@ -31,52 +31,49 @@ class ParserSpec extends SpecificationWithJUnit with Mockito {
     }
   }
 
-  val HTML_SIMPLE_TABLE =
-    <html>
-      <table class="master">
-        <tr>
-          <td>Mixins</td>
-          <td>org.pettswood.specification.groups.PettswoodBootstrap</td>
-        </tr>
-      </table>
-      <table>
-        <tr>
-          <td>Hello</td>
-        </tr>
-        <tr>
-          <td>Sausage</td>
-        </tr>
-      </table>
-    </html>
-
   "when html contains tables, the parser" should {
-    "delegate table handling to the domain" in {
+    "delegate table, row and cell handling to the domain" in {
       val fixture = new Fixture()
 
-      new Parser(fixture.domain).parse(HTML_SIMPLE_TABLE)
+      new Parser(fixture.domain).parse(
+        <table>
+          <tr>
+            <td>Hello</td>
+            <td>World</td>
+          </tr>
+        </table>
+      )
 
-      there was one(fixture.domain).table("Mixins")
       there was one(fixture.domain).table("Hello")
-      there were 4.times(fixture.domain).cell(any[String])
+      there was one(fixture.domain).row()
+      there was one(fixture.domain).cell("World")
     }
-    "jang pass/fail classes janged into the output" in {
+    "jang pass/fail classes into the output" in {
       val fixture = new Fixture()
-      fixture.domain.cell("Sausage") returns Fail("Sausage")
+      fixture.domain.cell("pass") returns Pass("pass")
+      fixture.domain.cell("fail") returns Fail("monkeys")
+      fixture.domain.cell("setup") returns Setup()
+      fixture.domain.cell("exception") returns Exception("the computer has gone to lunch")
 
-      val result = new Parser(fixture.domain).parse(HTML_SIMPLE_TABLE)
-
-      (result \\ "td").head must be equalTo <td class="Pass">Mixins</td>
-      (result \\ "td").reverse.head must be equalTo <td class="Fail">Sausage</td>
+      new Parser(fixture.domain).parse(<td>pass</td>) must be equalTo <td class="Pass">pass</td>
+      new Parser(fixture.domain).parse(<td>fail</td>) must be equalTo <td class="Fail">fail</td>
+      new Parser(fixture.domain).parse(<td>setup</td>) must be equalTo <td class="Setup">setup</td>
+      new Parser(fixture.domain).parse(<td>exception</td>) must be equalTo <td class="Exception">exception</td>
     }
-    "tell the domain when a new row is encountered" in {
+    "add a roll-up for failure results" in {
       val fixture = new Fixture()
+      fixture.domain.cell("expected") returns Fail("actual")
 
-      new Parser(fixture.domain).parse(HTML_SIMPLE_TABLE)
-
-      there were three(fixture.domain).row()
+      new Parser(fixture.domain).parse(<td>expected</td>) must be equalTo
+        <td>
+          <span class="calloutLink">expected</span> <div class="callout" title="Sausage">actual</div>
+        </td>
     }
-    "respect existing classes" in  {
-      Failure("Test failed due to lack of testyness. Infinite monkeys required")
+    "respect existing classes" in {
+      val fixture = new Fixture()
+      fixture.domain.cell("expected") returns Pass("actual")
+
+      new Parser(fixture.domain).parse(<td class="displayElegantly">expected</td>) must be equalTo <td class="displayElegantly Pass">expected</td>
     }
   }
 
@@ -120,18 +117,6 @@ class ParserSpec extends SpecificationWithJUnit with Mockito {
     }
     "detect the nested table when it parses the cell and stack the parent table" in {
       Failure("Test failed due to lack of testyness. Infinite monkeys required")
-    }
-  }
-
-  "The Parser" should {
-    "add a roll-up for failure results" in {
-      val fixture = new Fixture()
-      fixture.domain.cell("expected") returns Fail("actual")
-
-      new Parser(fixture.domain).parse(<td>expected</td>) must be equalTo
-        <td>
-          <span class="calloutLink">expected</span><div class="callout" title="Sausage">actual</div>
-        </td>
     }
   }
 }
