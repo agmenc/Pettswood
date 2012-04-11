a123b0e23aa8a618f116e76b116e365624499ef9
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2012-04-11 22:10:59 +0100

    Polishing docs, part 8 of n

--- javascript/pettswood.js
+++ javascript/pettswood.js
@@ -1,16 +1,14 @@
 //
 
 $(document).ready(function () {
-    $("a.calloutLink").each(function () {
-        var className = $(this).text().replace(/\.scala/g, "");
-        var $callout = $("pre#" + className);
-        $callout.hide();
-        $(this).click(function (event) {
-            $callout.toggle();
-            event.preventDefault();
-        })
+    $("h1").click(function() {
+        var url = window.location.href;
+        window.location = swap(url);
     });
+
+    function swap(url) {
+        if (url.match(new RegExp("src/test/resources"))) return url.replace("src/test/resources", "target/pettswood");
+        if (url.match(new RegExp("target/pettswood"))) return url.replace("target/pettswood", "src/test/resources");
+        throw "This document appears to be neither the source nor the result of a Pettswood test"
+    }
 });
-
-
-// Toggle
