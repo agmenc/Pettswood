8d6443c329eff9664fa83699962f596842987194
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2012-04-15 18:04:14 +0100

    Draft of presentation polished a little

--- notes/ScalaDays/slideshow.js
+++ notes/ScalaDays/slideshow.js
@@ -5,6 +5,18 @@ $(document).ready(function () {
     var slides = $("iframe");
     show();
 
+    $("#timer").click(function() {
+        colour();
+    });
+
+    function colour() {
+        var uncoloured = $("#timer").find("td").filter(function() { return !$(this).hasClass("gone"); });
+        if (uncoloured && uncoloured.length > 0) {
+            uncoloured.first().addClass("gone");
+            setTimeout(colour, 1000 * 60 * 5);
+        }
+    }
+
     $('body').keydown(function($event) {
         var key = $event.which;
         if (key === 37 || key === 39) {
