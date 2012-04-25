65542ac3e8118f3fb6530128c8c88cdcdfaf3f85
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2012-04-25 21:56:49 +0100

    Fixed index and automated doc publishing

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-cp -rf src/test/resources/* ../pettswood-docs/
+docs=../pettswood-docs
+rm -rf $docs/*
+cp -Rf index.html $docs
+mkdir -p $docs/src/test/resources
+cp -Rf src/test/resources $docs/src/test
+
+cd $docs
+git add .
+git commit -m "docs update"
+git push origin gh-pages
