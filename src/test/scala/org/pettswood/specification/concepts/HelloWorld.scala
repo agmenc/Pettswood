a9fe6d74abc94ee378063a856bf19b2631b54703
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2012-04-28 20:54:34 +0100

    Make test work.

--- src/test/scala/org/pettswood/specification/concepts/HelloWorld.scala
+++ src/test/scala/org/pettswood/specification/concepts/HelloWorld.scala
@@ -3,6 +3,6 @@ package org.pettswood.specification.concepts
 import org.pettswood.{Result, Concept}
 
 class HelloWorld extends Concept {
-  protected def cell(text: String) = Result.given("world", text)
+  protected def cell(text: String) = Result.given(text, "World")
 }
 
