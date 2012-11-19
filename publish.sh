4698f8d2eba55a32f98c55765e2ebace3d474e87
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2012-11-19 08:10:27 +0000

    Fix error in user guide. Remind myself how to publish stuff to Maven.

--- publish.sh
+++ publish.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 ./sbt test && git push origin master && ./sbt "+ publish"
+
+echo "."
+echo "Now release via Nexus/Sonatype"
+echo " - Log in as me"
+echo " - go to the STAGING repositories"
+echo " - Close the repo"
+echo " - Release the repo"
+echo " - Wait a couple of hours"
+echo "."
