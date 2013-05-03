cf94bcda6110659fb20af9e0781aa30ac10059ce
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2013-05-03 17:56:15 +0100

    Change HTML cleaning rules so that <script> tags in the <head> are ended in a browser-friendly way.

--- src/main/scala/org/pettswood/parsers/HtmlToXml.scala
+++ src/main/scala/org/pettswood/parsers/HtmlToXml.scala
@@ -1,14 +1,16 @@
 package org.pettswood.parsers
 
 import scala.xml._
-import org.htmlcleaner.{PrettyXmlSerializer, HtmlCleaner, CleanerProperties}
+import org.htmlcleaner.{PrettyHtmlSerializer, HtmlCleaner, CleanerProperties}
 
 object HtmlToXml {
-  def apply(input: String):Node = {
-    val props = new CleanerProperties();
-    props.setTranslateSpecialEntities(true);
-    val tagNode = new HtmlCleaner(props).clean(input);
-    val asString = new PrettyXmlSerializer(props).getAsString(tagNode)
+  def apply(input: String): Node = {
+    val props = new CleanerProperties()
+    props.setOmitXmlDeclaration(true)
+    props.setUseEmptyElementTags(false)
+    props.setTranslateSpecialEntities(true)
+    val tagNode = new HtmlCleaner(props).clean(input)
+    val asString = new PrettyHtmlSerializer(props).getAsString(tagNode)
     XML.loadString(asString)
   }
 }
--- src/test/scala/org/pettswood/parsers/HtmlToXmlSpec.scala
+++ src/test/scala/org/pettswood/parsers/HtmlToXmlSpec.scala
@@ -1,27 +1,49 @@
 package org.pettswood.parsers
 
 import org.specs2.mutable.Specification
+import scala.xml.NodeSeq
 
 class HtmlToXmlSpec extends Specification {
-  "dodgy HTML is converted to well-formed XML" in {
+  "XML entities are converted to unicode, so that Scala can parse them" in {
+    val input = "<div>&larr;&rarr;</div>"
+
+    val result = HtmlToXml(input)
+
+    result === wrap(<div>←→</div>)
+  }
+
+  "<br> tags are giving a closing /" in {
+    val input = "<br>"
+
+    val result = HtmlToXml(input)
+
+    result === wrap(<br/>)
+  }
+
+  "Script tags retain their closing tag, so that browsers don't ignore them" in {
     val dodgyHtml = "" +
       "<html>" +
       "    <head>" +
-      "        <link href=\"css/pettswood.css\" rel=\"stylesheet\" type=\"text/css\">" +
+      "        <script type=\"text/javascript\" src=\"javascript/jquery-1.7.2.min.js\"></script>" +
       "    </head>" +
-      "    <body>&larr;&rarr; <br></body>" +
       "</html>"
 
-    HtmlToXml(dodgyHtml) must ==/ (
+    val result = HtmlToXml(dodgyHtml)
+
+    val expected =
       <html>
         <head>
-          <link href="css/pettswood.css" type="text/css" rel="stylesheet"></link>
+          <script type="text/javascript" src="javascript/jquery-1.7.2.min.js"></script>
         </head>
-        <body>
-          ←→
-          <br></br>
-        </body>
+        <body></body>
       </html>
-    )
+
+    result === expected
+  }
+
+  def wrap(ns: NodeSeq): NodeSeq = <html><head/><body>{ns}</body></html>
+
+  implicit class EquableNodeSeq(ns: NodeSeq) {
+    def ===(other: NodeSeq) = ns must be_==/(other)
   }
 }
