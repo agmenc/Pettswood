032efa0805efb0082f357cc863f41831a576b2c9
agmenc <chris@Christopher-Agmen-Smiths-MacBook-Pro.local>
2013-05-03 17:56:52 +0100

    Add a doAll method, as a stop-gap while I decide how (and if) to combine probes

--- src/main/scala/org/pettswood/package.scala
+++ src/main/scala/org/pettswood/package.scala
@@ -2,7 +2,14 @@ package org
 
 package object pettswood {
   def doThis(f: (String) => Unit) = (text: String) => new Doer {f(text)}
+
   def dig(f: (String) => String) = (text: String) => new Digger {
     def actual = f(text)
+  }
+
+  // TODO - CAS - 26/04/2013 - Convert this to Probe.and(), e.g.:
+  // doThis() and doThis() and dig()
+  def doAll(doers: (String => Doer)*) = (text: String) => new Doer {
+    doers.foreach(_(text))
   }
 }
--- src/main/scala/org/pettswood/runners/sbt/PettswoodFramework.scala
+++ src/main/scala/org/pettswood/runners/sbt/PettswoodFramework.scala
@@ -6,12 +6,13 @@ import org.pettswood.files._
 
 class PettswoodFramework extends Framework {
   def name() = "Pettswood"
-  def tests = Array[Fingerprint](new PettswoodFingerprint(false), new PettswoodFingerprint(true))
+  def tests = Array[Fingerprint](new PettswoodFingerprint)
   def testRunner(classLoader: ClassLoader, loggers: Array[Logger]) = new Sbt(classLoader, loggers, DefaultRunner, DefaultPettswoodFiles)
 }
 
-class PettswoodFingerprint(override val isModule: Boolean) extends SubclassFingerprint {
+class PettswoodFingerprint extends SubclassFingerprint {
   def superClassName = "org.pettswood.runners.sbt.SbtIntegrationHook"
+  def isModule = false
 }
 
 trait SbtIntegrationHook
