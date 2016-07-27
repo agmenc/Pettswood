8255aae322a6049c263f542e158e8ea709b58aa4
agmenc <chris.agmen-smith@email.com>
2016-07-27 08:57:34 +0100

    Make state existence optional, so you don't have to provide a default everywhere

--- src/main/scala/org/pettswood/State.scala
+++ src/main/scala/org/pettswood/State.scala
@@ -1,14 +1,31 @@
 package org.pettswood
 
 import scala.collection.mutable
-import scala.reflect.ClassTag
+import scala.reflect.{ClassTag, classTag}
 
 class State {
   val stateMap = mutable.Map[Class[_], Any]()
+  val classOfNothing = classOf[scala.runtime.Nothing$]
 
   def retrieve[T : ClassTag](default: () => T) = {
-    val key = implicitly[ClassTag[T]].runtimeClass
+    val key = tag()
     if (!stateMap.contains(key)) stateMap += key -> default()
-    stateMap(key).asInstanceOf[T]
+    stateMap(tag()).asInstanceOf[T]
   }
+
+  def retrieve[T : ClassTag](): Option[T] = {
+    val key: Class[_] = tag[T]()
+
+    if (key.isAssignableFrom(classOfNothing)) throw new MissingTypeParameterException(classOf[State], "retrieve")
+
+    if (stateMap.contains(key))
+      Some(stateMap(key).asInstanceOf[T])
+    else
+      None
+  }
+
+  def tag[T: ClassTag](): Class[_] = classTag[T].runtimeClass
 }
+
+class MissingTypeParameterException(clazz: Class[_], methodName: String)
+  extends IllegalArgumentException(s"Missing type parameter in call to ${clazz.getCanonicalName}.$methodName")
--- src/test/scala/org/pettswood/specification/concepts/ShowCount.scala
+++ src/test/scala/org/pettswood/specification/concepts/ShowCount.scala
@@ -2,19 +2,27 @@ package org.pettswood.specification.concepts
 
 import org.pettswood._
 
+// Provides a default stateful class instance, in case one doesn't exist
 class ShowCount(state: State) extends Concept {
-  val counter = state.retrieve(() => new StatefulCounter)
+  lazy val counter: StatefulCounter = state.retrieve(() => new StatefulCounter)
 
   def cell(text: String) = Result.given(text, counter.count.toString)
 }
 
+// Assumes state already exists, but handles the error when it hasn't
 class Increment(state: State) extends Concept {
-  val counter = state.retrieve(() => new StatefulCounter)
+  lazy val counter: Option[StatefulCounter] = state.retrieve[StatefulCounter]()
 
   def cell(text: String) = text match {
     case "times" => Setup()
-    case number => counter.increment(number.toInt); Setup()
+    case number => incrementCounter(number)
   }
+
+  def incrementCounter(number: String): Result =
+    counter match {
+      case Some(c) => c.increment(number.toInt); Setup()
+      case None => Fail("No counter has been created for this test")
+    }
 }
 
 class StatefulCounter {
